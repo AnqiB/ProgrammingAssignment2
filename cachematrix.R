432d8ac3a883f6a6288ed36228b9085802cd555f
Angie Bai <anqib48356@foxmail.com>
2014-07-27 19:55:18 +0800

    A test example of the two functions which is given by Angelo Peñaloza Salazar have been described.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## makeCacheMatrix gives a matrix x four functions to manipulate, the operator "<<-" has not
-## been defined accurately, so the functions in this file may not really work 
-## cacheSolve tries to retrieve the inverse of matrix x from some cached zone. And if it fails
-## it will calculate the inverse of x and store newly got value in the cached zone.     
+## makeCacheMatrix gives a matrix x four functions to manipulate.
+## cacheSolve tries to retrieve the inverse of matrix x from the environment in which the two functions are defined. And if it fails
+## it will calculate the inverse of x and store newly got value in the cached zone.
+## '<<-' has been already defined in R.
 
 makeCacheMatrix <- function(x = matrix()) {
      Inver<-NULL  ## initialization of Inver
@@ -31,3 +31,45 @@ cacheSolve <- function(x, ...) {
 		x$setInverse(Inver)
 		Inver  ## Return a matrix that is the inverse of 'x'
 }
+
+## The following test example and method is provided by Angelo Peñaloza Salazar on the class forum. 
+#This is a test example to evaluate functions makeCacheMatrix and cacheSolve
+
+#1. Run both functions on Rstudio console
+
+#2. Define a 2x2 matrix called M
+#> M <- matrix(1:4, ncol=2, nrow=2)
+
+#3. Store the value of running makeCacheMatrix(M) which is a list.
+#> cacheM <- makeCacheMatrix(M)
+
+#4. Compute, cache, and return the inverse of matrix M
+#> cacheSolve(cacheM)
+
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+#5. Show the original matrix stored
+#> cacheM$get() 
+
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+
+#6. Show the inverse of the matrix stored
+#> cacheM$getInverse()
+
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+#7. Show the inverse of M called from cache
+#> cacheSolve(cacheM)  
+
+#getting cached data
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+
